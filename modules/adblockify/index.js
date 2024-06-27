645f958a0dda8e1fdf780ae025ebee7c503bed59
ririxi <dev@ririxi.dev>
2024-06-27 09:55:53 +0200

    feat: move to `modules` folder

---
+++ modules/adblockify/index.js
@@ -0,0 +1,32 @@
+import { Platform, Cosmos } from "/modules/official/stdlib/src/expose/Platform.js";
+import { modules, exportedFunctions } from "/modules/official/stdlib/src/webpack/index.js";
+import { getSettingsClient, getSlotsClient } from "./src/utils/clients.js";
+import { bindSlots, configureAdManagers, intervalUpdateSlotSettings } from "./src/slot.js";
+import { createLogger } from "/modules/official/stdlib/index.js";
+const { getAdManagers, getUserAPI } = Platform;
+export const adManagers = getAdManagers();
+export const productState = // @ts-expect-error: Depends on the version of Spotify, AutoGen doesn't have all of them
+getUserAPI()?._product_state || getUserAPI()._product_state_service || Platform?.getProductStateAPI()?.productStateApi;
+// @ts-expect-error: Apparently Cosmos expects two arguments, but the second argument is body which is optional
+export const slots = await Cosmos.get("sp://ads/v1/slots");
+export const settingsClient = getSettingsClient(modules);
+export const slotsClient = getSlotsClient(exportedFunctions, productState.transport);
+export let logger;
+// @ts-expect-error: I don't know.
+export default function(mod) {
+    logger = createLogger(mod);
+    /**
+	 * Main functions
+	 */ bindSlots(slots);
+    productState.subValues({
+        keys: [
+            "ads",
+            "catalogue",
+            "product",
+            "type"
+        ]
+    }, ()=>configureAdManagers());
+    logger.info("Loaded successfully");
+    // Update slot settings after 5 seconds... idk why, don't ask me why, it just works
+    setTimeout(intervalUpdateSlotSettings, 5 * 1000);
+}
